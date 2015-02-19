385e5be5cc432fa2191f845939076a55389695c8
Ozzie Ahmad <ahmad_ao2214@hotmail.com>
2015-02-19 04:58:07 -0600

    Added latitude and longitude coordinates

--- app/src/main/java/edu/depaul/scavi/activities/OzzieActivity.java
+++ app/src/main/java/edu/depaul/scavi/activities/OzzieActivity.java
@@ -1,25 +1,171 @@
 package edu.depaul.scavi.activities;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.ErrorDialogFragment;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import edu.depaul.scavi.R;
 
 /**
  * Created by matt on 2/15/15.
  */
-public class OzzieActivity extends Activity {
+public class OzzieActivity extends Activity
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     TextView latitudeView, longitudeView;
+    Location location;
+    //Request code to use when launching the resolution activity
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+    //Unique tag for the error dialog fragment
+    private static final String DIALOG_ERROR = "dialog_error";
+    private static final String STATE_RESOLVING_ERROR = "resolving_error";
+    //Bool to track whether the app is already resolving an error
+    private boolean mResolvingError = false;
+    private GoogleApiClient mGoogleApiClient;
+    private Bundle savedInstanceState;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ozzie);
 
         latitudeView = (TextView)findViewById(R.id.latitude);
         longitudeView = (TextView)findViewById(R.id.longitude);
+
+        // Create a GoogleApiClient instance
+        buildGoogleApiClient();
+
+        mResolvingError = savedInstanceState != null
+                && savedInstanceState.getBoolean(STATE_RESOLVING_ERROR, false);
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == REQUEST_RESOLVE_ERROR){
+            mResolvingError = false;
+            if (resultCode == RESULT_OK){
+                // Make sure the app is not already connected or attempting to connect
+                if(!mGoogleApiClient.isConnecting()&&
+                        !mGoogleApiClient.isConnected()){
+                    mGoogleApiClient.connect();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        // Connected to Google Play services!
+        // The good stuff goes here.
+        location = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (location != null){
+            latitudeView.setText(String.valueOf(location.getLatitude()));
+            longitudeView.setText(String.valueOf(location.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // The connection has been interrupted.
+        // Disable any UI components that depend on Google APIs
+        // until onConnected() is called.
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        // This callback is important for handling errors that
+        // may occur while attempting to connect with Google.
+        if (result.getErrorCode() == ConnectionResult.API_UNAVAILABLE){
+            // The android Wear app is not installed
+        }
+        if (mResolvingError){
+           // Already attempting to resolve an error.
+            return;
+        } else if (result.hasResolution()){
+            try{
+                mResolvingError = true;
+                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            } catch(IntentSender.SendIntentException e){
+                //There was an error with the resolution intent. Try again.
+                mGoogleApiClient.connect();
+            }
+
+        } else {
+            // Show dialog using GooglePlayServicesUtil.getErrorDialog()
+            showErrorDialog(result.getErrorCode());
+            mResolvingError = true;
+        }
+    }
+
+    /* Creates a dialog for an error message */
+    private void showErrorDialog(int errorCode) {
+        // Create a fragment for the error dialog
+        ErrorDialogFragment dialogFragment  = new ErrorDialogFragment();
+        // Pass the error that should be displayed
+        Bundle args = new Bundle();
+        args.putInt(DIALOG_ERROR, errorCode);
+        dialogFragment.setArguments(args);
+        dialogFragment.show(getFragmentManager(), "errordialog");
+    }
+
+    /* Called from ErrorDialogFragment when the dialog is dismissed. */
+    public void onDialogDismissed(){
+        mResolvingError = false;
+    }
+
+    public static class ErrorDialogFragment extends DialogFragment {
+        public ErrorDialogFragment(){ }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState){
+            // Get the error code and retrieve the appropriate dialog
+            int errorCode = this.getArguments().getInt(DIALOG_ERROR);
+            return GooglePlayServicesUtil.getErrorDialog(errorCode,
+                    this.getActivity(), REQUEST_RESOLVE_ERROR);
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog){
+            ((OzzieActivity)getActivity()).onDialogDismissed();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mResolvingError) {  // more about this later
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 }
